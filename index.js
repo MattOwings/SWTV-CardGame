e46cb88868c71f83285998a41f03f58a9e9b05d4
MattOwings <MattAlanOwings@gmail.com>
2022-03-26 15:23:21 -0500

    bare metal functionality

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+require("dotenv/config");
+require("./index.js"); // Grabs password from .env
+
+const client = new Discord.Client({
+    intents: ["GUILDS", "GUILD_MESSAGES"],
+});
+
+/* Bot Login */
+client.on("ready", () => {
+    console.log("SWTV Bot is now logged in");
+});
+
+client.on("message", (msg) => {
+    if (msg.channel.permissionsFor(msg.guild.me).has("SEND_MESSAGES")) {
+        if (msg.content.toLowerCase() === "hello there") {
+            msg.reply("General Kenobi");
+        }
+    }
+});
+
+client.login(process.env.TOKEN);
